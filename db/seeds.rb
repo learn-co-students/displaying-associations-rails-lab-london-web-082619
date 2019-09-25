6a9583d698838a6616fdd8ed696f84dc6a3f9334
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-09-25 17:08:11 +0100

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.create(name: "Adele")
+Artist.create(name: "Logic")
+Artist.create(name: "Stormzy")
+
+Song.create(title: "Set Fire", artist_id: 1)
+Song.create(title: "You were there", artist_id: 2)
+Song.create(title: "Song for you", artist_id: 3)
+Song.create(title: "Rage", artist_id: 1)
+Song.create(title: "Faces are clearer", artist_id: 2)
+Song.create(title: "Blah Blah", artist_id: 3)
